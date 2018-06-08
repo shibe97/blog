64cc8723570a5f7903ea5499da1b563039a08837
Kazuki Shibata <k.shibata97@gmail.com>
2018-06-08 11:18:20 +0900

    [update] styled-componentを導入

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import styled from "styled-components"
+
+const Item = styled.div`
+  padding: 1rem 2rem 1rem 0;
+  color: #333;
+`;
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-start">
-        <Link className="navbar-item" to="/about">
-          About
+        <Link to="/">
+          <Item>Blog</Item>
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
+        <Link to="/about">
+          <Item>About</Item>
+        </Link>
+        <Link to="/products">
+          <Item>Products</Item>
         </Link>
       </div>
       <div className="navbar-end">
