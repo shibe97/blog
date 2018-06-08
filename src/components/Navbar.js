29ff6907d0f2f96784f09ac03821faceb9716bae
Kazuki Shibata <k.shibata97@gmail.com>
2018-06-08 23:02:02 +0900

    [update] ヘッダーのスタイル変更

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,27 +2,47 @@ import React from 'react'
 import { navigateTo } from 'gatsby-link'
 import styled from "styled-components"
 
+import img from '../img/shibe97.png';
+
+const Nav = styled.nav`
+  margin: 0 auto;
+  width: calc(100% - 3rem);
+  display: flex;
+  align-items: center;
+`;
+
+const ProfImg = styled.img`
+  width: 40px;
+  height: 40px;
+`;
+
 const Item = styled.div`
-  margin: 1rem 2rem 1rem 0;
+  margin: 1rem 1.5rem 1rem 0;
   color: #333;
   cursor: pointer;
 `;
 
+const Line = styled.div`
+  width: 1px;
+  height: 40px;
+  background-color: #ccc;
+`;
+
 const Navbar = () => (
-  <nav className="navbar is-transparent">
-    <div className="container">
-      <div className="navbar-start">
-        <Item onClick={() => navigateTo('/')}>
-          Blog
-        </Item>
-        <Item onClick={() => navigateTo('/about')}>
-          About
-        </Item>
-      </div>
-      <div className="navbar-end">
-      </div>
-    </div>
-  </nav>
+  <Nav>
+    <Item onClick={() => navigateTo('/')}>
+      <ProfImg src={img} alt="shibe97" />
+    </Item>
+    <Item>
+      <Line />
+    </Item>
+    <Item onClick={() => navigateTo('/')}>
+      Blog
+    </Item>
+    <Item onClick={() => navigateTo('/about')}>
+      About
+    </Item>
+  </Nav>
 )
 
 export default Navbar
