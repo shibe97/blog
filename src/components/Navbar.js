33dd293b01e041b67696891a14cf7ef9b5db3ce1
Kazuki Shibata <k.shibata97@gmail.com>
2018-06-08 11:32:15 +0900

    [update] ヘッダーのスタイル調整

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,23 @@
 import React from 'react'
-import Link from 'gatsby-link'
+import { navigateTo } from 'gatsby-link'
 import styled from "styled-components"
 
 const Item = styled.div`
-  padding: 1rem 2rem 1rem 0;
+  margin: 1rem 2rem 1rem 0;
   color: #333;
+  cursor: pointer;
 `;
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-start">
-        <Link to="/">
-          <Item>Blog</Item>
-        </Link>
-        <Link to="/about">
-          <Item>About</Item>
-        </Link>
-        <Link to="/products">
-          <Item>Products</Item>
-        </Link>
+        <Item onClick={() => navigateTo('/')}>
+          Blog
+        </Item>
+        <Item onClick={() => navigateTo('/about')}>
+          About
+        </Item>
       </div>
       <div className="navbar-end">
       </div>
