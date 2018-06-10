bad88e518eaecb94056fcf715938bd8eb5fe5b06
Kazuki Shibata <k.shibata97@gmail.com>
2018-06-10 22:15:19 +0900

    [update] 画像の高さ、横幅を指定

--- src/components/Features.js
+++ src/components/Features.js
@@ -7,13 +7,18 @@ const Title = styled.p`
   font-weight: bold;
 `;
 
+const Img = styled.img`
+  max-height: 200px;
+  max-width: 200px;
+`;
+
 const FeatureGrid = ({ gridItems }) => (
   <div className="columns is-multiline">
     {gridItems.map(item => (
       <div key={item.image} className="column is-6">
         <section className="section">
           <p className="has-text-centered">
-            <img alt="" src={item.image} />
+            <Img alt="" src={item.image} />
           </p>
           <Title>{item.title}</Title>
           <p>{item.text}</p>
